b8cae1c9c5c1efb1d750a458df22d5af8d1499c1
prakash <prakash03445@gmail.com>
2025-12-10 07:13:27 +0530

    feat(db): Define and intialize 'urls'table schema
    - Defines the 'urls' table with 'short_code', 'long_url'

---
+++ db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    short_code VARCHAR(10) UNIQUE NOT NULL,
+    long_url TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_short_code ON urls (short_code);
